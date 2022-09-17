236bedef41b7613e44e7be85a4d536e99e8a3074
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2022-09-18 02:14:29 +0530

    menu toggle added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
 'use strict';
 
-const  elemToggleFunc = function (elem){elem.classlist.toggle("active")}
+const  elemToggleFunc = function (elem) {elem.classList.toggle("active")}
 
 // navbar variables
 const navbar = document.querySelector("[data-navbar]")
 const navbarToggleBtn =  document.querySelector("[data-navbar-toggle-btn]")
 
 
-navbarToggleBtn.addEventListener("click", function(){
-    elemToggleFunc(navbar);
-})
+navbarToggleBtn.addEventListener("click", function(){elemToggleFunc(navbar)});
