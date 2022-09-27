c92f55907e2afb85aea19cfd56978cbdad244ba1
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2022-09-28 00:02:13 +0530

    styling explore section

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,14 @@ const navbar = document.querySelector("[data-navbar]")
 const navbarToggleBtn =  document.querySelector("[data-navbar-toggle-btn]")
 
 
-navbarToggleBtn.addEventListener("click", function(){elemToggleFunc(navbar)});
+navbarToggleBtn.addEventListener("click", function(){elemToggleFunc(navbar);});
+
+// wishlist
+const whishlistBtn = document.querySelectorAll("[data-whishlist-btn]")
+
+
+for (let i = 0; i < whishlistBtn.length; i++) {
+
+    whishlistBtn[i].addEventListener("click", function () { elemToggleFunc(this); });
+  
+  }
